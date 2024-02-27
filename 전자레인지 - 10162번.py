eb5299ec75bc3b4e5fb60b8b1a9b0b5f2a71aabd
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-02-27 10:20:56 +0900

    Create 전자레인지 - 10162번.py

---
+++ 전자레인지 - 10162번.py
@@ -0,0 +1,14 @@
+#https://www.acmicpc.net/problem/10162
+#https://www.acmicpc.net/source/74061601
+
+t = int(input())
+
+if t % 10 != 0:
+    print(-1)
+else:
+    a = t // 300
+    t %= 300
+    b = t // 60
+    t %= 60
+    c = t // 10
+    print(a,b,c)
