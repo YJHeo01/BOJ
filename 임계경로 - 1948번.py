bb603d9ff0f770ee3ad29a9832888d124ff4d2a6
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-04-26 13:52:03 +0900

    Update 임계경로 - 1948번.py

--- 임계경로 - 1948번.py
+++ 임계경로 - 1948번.py
@@ -1,42 +1,47 @@
 #https://www.acmicpc.net/problem/1948
-#https://www.acmicpc.net/source/75410210
+#https://www.acmicpc.net/source/77492034
 
 from collections import deque
 import sys
 
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
-n = int(input())
-m = int(input())
 
-indegree = [0] * (n+1)
-dest_time = [0] * (n+1)
-slowest_path_road_cnt = [0] * (n+1)
-graph = [[] for _ in range(n+1)]
+def main():
+    indegree = [0] * (n+1)
+    graph = get_graph_indegree(indegree)
+    slowest_path_road_cnt = [0] * (n+1)
+    start,end = map(int,input().split())
+    dest_time = get_dest_time(graph,indegree,slowest_path_road_cnt,start)
+    time = dest_time[end]
+    visited_road = [[False]*(n+1) for _ in range(n+1)]
+    road_cnt = get_road_cnt(graph,visited_road,dest_time,start) - 1
+    print(time)
+    print(road_cnt)
 
-for _ in range(m):
-    a,b,c = map(int,input().split())
-    indegree[b] += 1
-    graph[a].append((b,c))
+def get_graph_indegree(indegree):
+    graph = [[] for _ in range(n+1)]
+    for _ in range(m):
+        a,b,c = map(int,input().split())
+        indegree[b] += 1
+        graph[a].append((b,c))
+    return graph
 
-start,end = map(int,input().split())
+def get_dest_time(graph,indegree,slowest_path_road_cnt,start):
+    queue = deque([start])
+    dest_time = [0] * (n+1)
+    while queue:
+        vx = queue.popleft()
+        for nx, time in graph[vx]:
+            if dest_time[vx] + time > dest_time[nx]:
+                dest_time[nx] = dest_time[vx] + time
+                slowest_path_road_cnt[nx] = slowest_path_road_cnt[vx] + 1
+            indegree[nx] -= 1
+            if indegree[nx] == 0:
+                queue.append(nx)
+    return dest_time
 
-queue = deque([start])
-
-while queue: #만나는 시간
-    vx = queue.popleft()
-    for nx, time in graph[vx]:
-        if dest_time[vx] + time > dest_time[nx]:
-            dest_time[nx] = dest_time[vx] + time
-            slowest_path_road_cnt[nx] = slowest_path_road_cnt[vx] + 1
-        elif dest_time[vx] + time == dest_time[nx]:
-            slowest_path_road_cnt[nx]
-        indegree[nx] -= 1
-        if indegree[nx] == 0:
-            queue.append(nx)
-print(dest_time[end])
-
-def solution(graph,visited_road,dest_time,start):
+def get_road_cnt(graph,visited_road,dest_time,start):
     if graph[start] == []:
         return 1
     ret_value = 0
@@ -44,7 +49,7 @@ def solution(graph,visited_road,dest_time,start):
     for nx, time in graph[start]:
         if dest_time[nx] == dest_time[start] + time:
             if visited_road[nx][start] == False:
-                tmp = solution(graph,visited_road,dest_time,nx)
+                tmp = get_road_cnt(graph,visited_road,dest_time,nx)
                 if tmp > 0:
                     ret_value += tmp
                     visited_road[nx][start] = True
@@ -56,6 +61,8 @@ def solution(graph,visited_road,dest_time,start):
     if ret_value == 0 and one == True:
         ret_value = 1
     return ret_value
-visited_road = [[False]*(n+1) for _ in range(n+1)]
-second_line_answer = solution(graph,visited_road,dest_time,start) - 1
-print(second_line_answer)
+
+if __name__ == "__main__":
+    n = int(input())
+    m = int(input())
+    main()
