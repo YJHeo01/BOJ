bd5ab4ab8fa81db32d98bcdeb6e0f5336e745ddd
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-04-26 14:32:31 +0900

    가독성 향상 (백조의 호수 - 3197번.py)

--- 백조의 호수 - 3197번.py
+++ 백조의 호수 - 3197번.py
@@ -1,5 +1,5 @@
 #https://www.acmicpc.net/problem/3197
-#https://www.acmicpc.net/source/76393803
+#https://www.acmicpc.net/source/77493461
 #pypy3
 
 from collections import deque
@@ -7,63 +7,76 @@ import sys
 
 input = sys.stdin.readline
 
-r,c = map(int,input().split())
-
-lake = []
-
-for _ in range(r):
-    lake.append(list(input()))
-
-melt_time = [[0]*c for _ in range(r)]
-
-start = (-1,-1)
-end = (-1,-1)
-
 INF = int(1e9)
 
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
+def main():
+    lake = get_lake()
+    melt_time = [[0]*c for _ in range(r)]
+    visited = [[INF]*c for _ in range(r)]
+    edge = get_edge(lake,melt_time)
+    start,end = get_start_end(lake)
+    init_melt_time(melt_time,edge)
+    solution(melt_time,visited,start)
+    answer = visited[end[0]][end[1]]
+    print(answer)
 
-bfs_start = []
+def get_lake():
+    lake = []
+    for _ in range(r):
+        lake.append(list(input()))
+    return lake
 
-for x in range(r):
-    for y in range(c):
-        if lake[x][y] == 'X':
-            melt_time[x][y] = INF
-        else:
-            if lake[x][y] == 'L':
-                if start == (-1,-1):
-                    start = (x,y)
-                else:
-                    end = (x,y)
+def get_edge(lake,melt_time):
+    dx = [0,1,0,-1]
+    dy = [1,0,-1,0]
+    ret_value = []
+    for x in range(r):
+        for y in range(c):
+            if lake[x][y] == 'X':
+                melt_time[x][y] = INF
+                continue
             for k in range(4):
                 nx = x + dx[k]
                 ny = y + dy[k]
-                if nx < 0 or ny < 0 or nx >= r or ny >= c:
+                if exit_lake(nx,ny):
                     continue
                 if lake[nx][ny] == 'X':
-                    bfs_start.append((x,y))
+                    ret_value.append((x,y))
                     break
-    
-def bfs(visited,start):
-    queue = deque(start)
+    return ret_value
+
+def exit_lake(x,y):
+    if x < 0 or y < 0 or x >= r or y >= c:
+        return True
+    return False
+
+def get_start_end(lake):
+    start = (-1,-1)
+    for i in range(r):
+        for j in range(c):
+            if lake[i][j] == 'L':
+                if start == (-1,-1):
+                    start = (i,j)
+                else:
+                    end = (i,j)
+                    return start,end
+    return start,end
+
+def init_melt_time(melt_time,edge):
+    queue = deque(edge)
     dx = [0,1,0,-1]
     dy = [1,0,-1,0]
     while queue:
-        vx, vy = queue.popleft()
+        vx,vy = queue.popleft()
         for i in range(4):
             nx = vx + dx[i]
             ny = vy + dy[i]
-            if nx < 0 or ny < 0 or nx >= r or ny >= c:
+            if exit_lake(nx,ny):
                 continue
-            if visited[nx][ny] > visited[vx][vy] + 1:
-                visited[nx][ny] = visited[vx][vy] + 1
+            if melt_time[nx][ny] > melt_time[vx][vy] + 1:
+                melt_time[nx][ny] = melt_time[vx][vy] + 1
                 queue.append((nx,ny))
-
-bfs(melt_time,bfs_start)
-
-visited = [[INF]*c for _ in range(r)]
-
+    
 def solution(melt_time,visited,start):
     queue = deque([start])
     dx = [0,1,0,-1]
@@ -74,14 +87,12 @@ def solution(melt_time,visited,start):
         for i in range(4):
             nx = vx + dx[i]
             ny = vy + dy[i]
-            if nx < 0 or ny < 0 or nx >= r or ny >= c:
+            if exit_lake(nx,ny):
                 continue
             if visited[nx][ny] > max(visited[vx][vy],melt_time[nx][ny]):
                 visited[nx][ny] = max(visited[vx][vy],melt_time[nx][ny])
                 queue.append((nx,ny))
 
-solution(melt_time,visited,start)
-
-answer = visited[end[0]][end[1]]
-
-print(answer)
+if __name__ == "__main__":
+    r,c = map(int,input().split())
+    main()
