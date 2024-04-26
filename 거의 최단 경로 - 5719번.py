749d2f97a0b5a70e4b99f51a82c7f8d58e1625fe
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-04-26 14:48:19 +0900

    가독성 향상(거의 최단 경로 - 5719번.py)

--- 거의 최단 경로 - 5719번.py
+++ 거의 최단 경로 - 5719번.py
@@ -1,5 +1,5 @@
 #https://www.acmicpc.net/problem/5719
-#https://www.acmicpc.net/source/75255403
+#https://www.acmicpc.net/source/77494106
 
 from collections import deque
 import sys,heapq
@@ -7,6 +7,27 @@ import sys,heapq
 input = sys.stdin.readline
 
 INF = int(1e9)
+
+def main():
+    graph, reverse_graph = get_graph()
+    distance = [INF] * n
+    get_shortest_path(graph,distance,s)
+    shortest_path_edge = get_shortest_path_edge(reverse_graph,distance)
+    distance = [INF] * n
+    solution(graph,distance,shortest_path_edge)
+    answer = distance[d]
+    if answer >= INF:
+        answer = -1
+    print(answer)
+
+def get_graph():
+    graph = [[] for _ in range(n)]
+    reverse_graph = [[] for _ in range(n)]
+    for _ in range(m):
+        u,v,p = map(int,input().split())
+        graph[u].append((v,p))
+        reverse_graph[v].append((u,p))
+    return graph,reverse_graph
 
 def get_shortest_path(graph,distance,start):
     q = []
@@ -17,24 +38,24 @@ def get_shortest_path(graph,distance,start):
         if dist > distance[vx]:
             continue
         for nx, length in graph[vx]:
-            if block_road[vx][nx] == True:
-                continue
             if distance[nx] > dist + length:
                 distance[nx] = dist + length
                 heapq.heappush(q,(distance[nx],nx))
 
-def get_block_road(graph,distance,block_road):
+def get_shortest_path_edge(graph,distance):
     queue = deque([d])
+    shortest_path_edge = [[False]*n for _ in range(n)]
     while queue:
         vx = queue.popleft()
         for nx, length in graph[vx]:
-            if block_road[nx][vx] == True:
+            if shortest_path_edge[nx][vx] == True:
                 continue
             if distance[vx] == distance[nx] + length:
-                block_road[nx][vx] = True
+                shortest_path_edge[nx][vx] = True
                 queue.append(nx)
+    return shortest_path_edge
 
-def solution(graph,distance,block_road):
+def solution(graph,distance,shortest_path_edge):
     q = []
     heapq.heappush(q,(0,s))
     distance[s] = 0
@@ -43,31 +64,16 @@ def solution(graph,distance,block_road):
         if dist > distance[vx]:
             continue
         for nx, length in graph[vx]:
-            if block_road[vx][nx] == True:
+            if shortest_path_edge[vx][nx] == True:
                 continue
             if distance[nx] > dist + length:
                 distance[nx] = dist + length
                 heapq.heappush(q,(distance[nx],nx))
 
-while True:
-    n,m = map(int,input().split())
-    if n == 0:
-        break
-    s,d = map(int,input().split())
-    graph = [[] for _ in range(n)]
-    graph_reverse = [[] for _ in range(n)]
-    for _ in range(m):
-        u,v,p = map(int,input().split())
-        graph[u].append((v,p))
-        graph_reverse[v].append((u,p))
-    distance = [INF] * n
-    block_road = [[False]*n for _ in range(n)]
-    get_shortest_path(graph,distance,s)
-    original_length = distance[d]
-    get_block_road(graph_reverse,distance,block_road)
-    distance = [INF] * n
-    solution(graph,distance,block_road)
-    answer = distance[d]
-    if answer >= INF:
-        answer = -1
-    print(answer)
+if __name__ == "__main__":
+    while True:
+        n,m = map(int,input().split())
+        if n == 0:
+            break
+        s,d = map(int,input().split())
+        main()
