ca5745a7f36130429c68891b623d133eef99bf3d
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-04-26 13:06:24 +0900

    가독성 개선 (22870번.py)

--- 산책 (large) - 22870번.py
+++ 산책 (large) - 22870번.py
@@ -1,5 +1,5 @@
 #https://www.acmicpc.net/problem/22870
-#https://www.acmicpc.net/source/77486307
+#https://www.acmicpc.net/source/77490679
 
 import sys, heapq
 
@@ -8,20 +8,10 @@ input = sys.stdin.readline
 INF = int(1e9)
 
 def main():
-    global n,m,s,e
-    n,m = map(int,input().split())
-    graph = [[] for _ in range(n+1)]
-    for _ in range(m):
-        a,b,c = map(int,input().split())
-        graph[a].append((b,c))
-        graph[b].append((a,c))
-    for i in range(1,n+1):
-        graph[i].sort()
-    s,e = map(int,input().split())
     distance = [INF] * (n+1)
     dijkstra_start(graph,distance,e)
     answer = distance[s]
-    shortest_path = search_shortest_path(graph,distance,[s])
+    shortest_path = get_shortest_path(graph,distance,[s])
     shortest_path.pop()
     distance = [INF] * (n+1)
     dijkstra_return(graph,distance,shortest_path,e)
@@ -41,15 +31,15 @@ def dijkstra_start(graph,distance,start):
                 distance[nx] = dist + length
                 heapq.heappush(q,(distance[nx],nx))
 
-def search_shortest_path(graph,distance,path):
+def get_shortest_path(graph,distance,path):
     vx = path[0]
     if vx == e:
         return path
     for nx, length in graph[vx]:
         if distance[nx] + length == distance[vx]:
-            return search_shortest_path(graph,distance,[nx]+path)
+            return get_shortest_path(graph,distance,[nx]+path)
 
-def dijkstra_return(graph,distance,prohabit_node,start):
+def dijkstra_return(graph,distance,shortest_path,start):
     q = []
     heapq.heappush(q,(0,start))
     distance[start] = 0
@@ -58,11 +48,24 @@ def dijkstra_return(graph,distance,prohabit_node,start):
         if dist > distance[vx]:
             continue
         for nx, length in graph[vx]:
-            if nx in prohabit_node:
+            if nx in shortest_path:
                 continue
             if distance[nx] > dist + length:
                 distance[nx] = dist + length
                 heapq.heappush(q,(distance[nx],nx))
 
+def get_graph():
+    graph = [[] for _ in range(n+1)]
+    for _ in range(m):
+        a,b,c = map(int,input().split())
+        graph[a].append((b,c))
+        graph[b].append((a,c))
+    for i in range(1,n+1):
+        graph[i].sort()
+    return graph
+
 if __name__ == "__main__":
+    n,m = map(int,input().split())
+    graph = get_graph()
+    s,e = map(int,input().split())
     main()
