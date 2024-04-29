91dd65a1b942005bf69f3681bde69a2b25379bc9
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-04-29 12:24:06 +0900

    Update 게임 - 1103번.py

--- 게임 - 1103번.py
+++ 게임 - 1103번.py
@@ -1,45 +1,55 @@
 #https://www.acmicpc.net/problem/1103
-#https://www.acmicpc.net/source/77598908
-#pypy3
+#https://www.acmicpc.net/source/77599756
 
-n,m = map(int,input().split())
+import sys
 
-board = []
-for _ in range(n):
-    board.append(list(input()))
-
-for i in range(n):
-    for j in range(m):
-        if board[i][j] != 'H':
-            board[i][j] = int(board[i][j])
-
+input = sys.stdin.readline
 
 INF = int(1e9)
 
-dp = [[INF]*m for _ in range(n)]
+def main():
+    board = get_board()
+    init_board(board)
+    dp = [[INF]*m for _ in range(n)]
+    dp[0][0] = 0
+    answer = solution(board,dp)
+    if answer == 2501:
+        answer = -1
+    print(answer)
 
-dp[0][0] = 0
-answer = 0
+def get_board():
+    board = []
+    for _ in range(n):
+        board.append(list(input()))
+    return board
 
-for i in range(2501):
-    if answer != i:
-        answer += 1
-        break
-    for vx in range(n):
-        for vy in range(m):
-            if board[vx][vy] == 'H' or dp[vx][vy] != i:
-                continue
-            dx = [board[vx][vy],-board[vx][vy],0,0]
-            dy = [0,0,board[vx][vy],-board[vx][vy]]
-            for k in range(4):
-                nx = vx + dx[k]
-                ny = vy + dy[k]
-                if nx < 0 or ny < 0 or nx >= n or ny >= m or board[nx][ny] == 'H':
+def init_board(board):
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] != 'H':
+                board[i][j] = int(board[i][j])
+
+def solution(board,dp):
+    answer = 0
+    for i in range(2501):
+        for vx in range(n):
+            for vy in range(m):
+                if board[vx][vy] == 'H' or dp[vx][vy] != i:
                     continue
-                dp[nx][ny] = i + 1
-                answer = i + 1
+                dx = [board[vx][vy],-board[vx][vy],0,0]
+                dy = [0,0,board[vx][vy],-board[vx][vy]]
+                for k in range(4):
+                    nx = vx + dx[k]
+                    ny = vy + dy[k]
+                    if nx < 0 or ny < 0 or nx >= n or ny >= m or board[nx][ny] == 'H':
+                        continue
+                    dp[nx][ny] = i + 1
+                    answer = i + 1
+        if answer == i:
+            answer += 1
+            break
+    return answer
 
-if answer == 2501:
-    answer = -1
-
-print(answer)
+if __name__ == "__main__":
+    n,m = map(int,input().split())
+    main()
