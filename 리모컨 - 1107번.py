1c5fb7a88c6d8216ac4d0f93ae4eabfa4a9d9437
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-05-09 14:58:10 +0900

    Update 리모컨 - 1107번.py

--- 리모컨 - 1107번.py
+++ 리모컨 - 1107번.py
@@ -1,64 +1,87 @@
 #https://www.acmicpc.net/problem/1107
-#https://www.acmicpc.net/source/74078543
+#https://www.acmicpc.net/source/78088749
 
 from collections import deque
 
 SIZE = 1000001
 
-n = int(input())
-m = int(input())
+def main():
+    global n
+    n = int(input())
+    m = int(input())
+    if m == 0:
+        answer = not_need_fix(n)
+        print(answer)
+        return
+    button = get_button()
+    click_cnt = [SIZE] * SIZE
+    answer = control(button,click_cnt)
+    print(answer)
 
-button = [True] * 10
-if m != 0:
-    tmp = list(map(int,input().split()))
-    for idx in tmp:
-        button[idx] = False
-
-button_list = []
-click_button_cnt = [SIZE] * SIZE
-start = []
-channel_list = [100]
-for i in range(10):
-    if button[i] == False:
-        continue
-    button_list.append(i)
-    click_button_cnt[i] = 1
-    start.append(i)
-    channel_list.append(i)
-
-
-def click_number_button(button_list,click_button_cnt,start):
-    queue = deque(start)
-    ret_value = []
-    while queue:
-        channel = queue.popleft()
-        tmp_channel = 10 * channel
-        for clicked_button in button_list:
-            next_channel = tmp_channel + clicked_button
-            if next_channel >= SIZE:
-                continue
-            if click_button_cnt[next_channel] > click_button_cnt[channel] + 1:
-                if click_button_cnt[next_channel] == SIZE:
-                    ret_value.append(next_channel)
-                click_button_cnt[next_channel] = click_button_cnt[channel] + 1
-                queue.append(next_channel)
+def not_need_fix(n):
+    ret_value = abs(n-100)
+    tmp = 1
+    while True:
+        n = n // 10
+        if n == 0:
+            break
+        tmp += 1
+    ret_value = min(ret_value,tmp)
     return ret_value
 
+def get_button():
+    button = [True] * 10
+    tmp = list(map(int,input().split()))
+    for i in tmp:
+        button[i] = False
+    button_list = []
+    for i in range(10):
+        if button[i] == False: continue
+        button_list.append(i)
+    return button_list
 
-channel_list += click_number_button(button_list,click_button_cnt,start)
-click_button_cnt[100] = 0
+def control(button,click_cnt):
+    channel_list = click_num_button(button,click_cnt)
+    click_plus_minus_button(click_cnt,channel_list)        
+    return click_cnt[n]
 
-def click_plus_minus_button(click_button_cnt,start):
-    queue = deque(start)
-    dx_list = [1,-1]
+def click_num_button(button,click_cnt):
+    queue = init_queue(button,click_cnt)
+    ret_value = [100]
     while queue:
-        channel = queue.popleft()
-        for dx in dx_list:
-            next_channel = channel + dx
-            if next_channel < 0 or next_channel >= SIZE:
-                continue
-            if click_button_cnt[next_channel] > click_button_cnt[channel] + 1:
-                click_button_cnt[next_channel] = click_button_cnt[channel] + 1
-                queue.append(next_channel)
-click_plus_minus_button(click_button_cnt,channel_list)
-print(click_button_cnt[n])
+        vx = queue.popleft()
+        ret_value.append(vx)
+        tmp = vx * 10
+        if tmp >= SIZE: continue
+        for dx in button:
+            nx = tmp + dx
+            if nx >= SIZE: break
+            if click_cnt[nx] > click_cnt[vx] + 1:
+                click_cnt[nx] = click_cnt[vx] + 1
+                queue.append(nx)
+    return ret_value
+
+def init_queue(button,click_cnt):
+    ret_value = deque([])
+    for i in button:
+        click_cnt[i] = 1
+        ret_value.append(i)
+    return ret_value
+
+def click_plus_minus_button(click_cnt,start):
+    queue = deque(start)
+    click_cnt[100] = 0
+    while queue:
+        vx = queue.popleft()
+        nx = get_nx(vx)
+        if click_cnt[nx] <= click_cnt[vx] + 1:continue
+        click_cnt[nx] = click_cnt[vx] + 1
+        queue.append(nx)
+
+def get_nx(vx):
+    if vx > n:
+        return vx - 1
+    return vx + 1
+
+if __name__ == "__main__": 
+    main()
