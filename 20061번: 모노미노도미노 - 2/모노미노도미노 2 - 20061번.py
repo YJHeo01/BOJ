fe5544deafc03a8e9f15ff68d30cd0b853ed2796
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-05-14 11:40:37 +0900

    Update 모노미노도미노 2 - 20061번.py

--- 20061번: 모노미노도미노 - 2/모노미노도미노 2 - 20061번.py
+++ 20061번: 모노미노도미노 - 2/모노미노도미노 2 - 20061번.py
@@ -1,14 +1,17 @@
 #https://www.acmicpc.net/problem/20061
-#https://www.acmicpc.net/source/77969671
+#https://www.acmicpc.net/source/78314130
 
 import sys
 
 input = sys.stdin.readline
 
 def main():
-    n = int(input())
     green = [[False]*4 for _ in range(6)]
     blue = [[False]*6 for _ in range(4)]
+    solution(green,blue)
+
+def solution(green,blue):
+    n = int(input())
     score = 0
     for _ in range(n):
         t,x,y = map(int,input().split())
@@ -33,20 +36,18 @@ def main():
 def get_new_blue_block(blue,x1,x2):
     y = 2
     while True:
-        if y > 5 or blue[x1][y] == True or blue[x2][y] == True:
-            y -= 1
-            break
+        if y > 5 or blue[x1][y] == True or blue[x2][y] == True: break
         y += 1
+    y -= 1
     blue[x1][y] = True; blue[x2][y] = True
     return y
 
 def get_new_green_block(green,y1,y2):
     x = 2
     while True:
-        if x > 5 or green[x][y1] == True or green[x][y2] == True:
-            x -= 1
-            break
+        if x > 5 or green[x][y1] == True or green[x][y2] == True: break
         x += 1
+    x -= 1
     green[x][y1] = True; green[x][y2] = True
     return x
 
@@ -54,8 +55,7 @@ def get_blue_board_score(blue):
     start = 5; end = -1; ret_value = 0
     for column in range(start,end,-1):
         while True:
-            if check_full_column(blue,column) == False:
-                break
+            if check_full_column(blue,column) == False: break
             ret_value += 1; end += 1
             erase_column(blue,column)
             move_blue_block(blue,column)
@@ -63,13 +63,11 @@ def get_blue_board_score(blue):
 
 def check_full_column(board,column):
     for row in range(4):
-        if board[row][column] == False:
-            return False
+        if board[row][column] == False: return False
     return True
 
 def erase_column(blue,column):
-    for row in range(4):
-        blue[row][column] = False
+    for row in range(4): blue[row][column] = False
     return blue
 
 def move_blue_block(blue,start_column):
@@ -84,16 +82,14 @@ def get_green_board_score(green):
     start = 5; end = -1; ret_value = 0
     for row in range(start,end,-1):
         while True:
-            if check_full_row(green,row) == False:
-                break
+            if check_full_row(green,row) == False: break
             ret_value += 1; end += 1
             erase_row(green,row)
             move_green_block(green,row)
     return ret_value
 
 def erase_row(green,row):
-    for column in range(4):
-        green[row][column] = False
+    for column in range(4): green[row][column] = False
     return green
 
 def move_green_block(green,start_row):
@@ -106,8 +102,7 @@ def move_green_block(green,start_row):
 
 def check_full_row(green,row):
     for column in range(4):
-        if green[row][column] == False:
-            return False
+        if green[row][column] == False: return False
     return True
 
 def special_row(green):
@@ -118,8 +113,7 @@ def special_row(green):
 
 def empty_row(green,row):
     for column in range(4):
-        if green[row][column] == True:
-            return False
+        if green[row][column] == True: return False
     return True
 
 def special_column(blue):
@@ -130,16 +124,14 @@ def special_column(blue):
 
 def empty_column(blue,column):
     for row in range(4):
-        if blue[row][column] == True:
-            return False
+        if blue[row][column] == True: return False
     return True
 
 def get_cnt(board):
     ret_value = 0
     for row in board:
         for column in row:
-            if column == True:
-                ret_value += 1
+            if column == True: ret_value += 1
     return ret_value
 
 if __name__ == "__main__":
