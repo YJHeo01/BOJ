c46837f6529c5fcfff6f5c8fa6fc21eec298d143
허용준 <93248202+YJHeo01@users.noreply.github.com>
2024-06-17 17:35:41 +0900

    Update 공항 - 10775번.py

--- 공항 - 10775번.py
+++ 공항 - 10775번.py
@@ -1,30 +1,30 @@
 #https://www.acmicpc.net/problem/10775
-#https://www.acmicpc.net/source/71876819
+#https://www.acmicpc.net/source/79730034
 
 import sys
 
+sys.setrecursionlimit(10**6)
+
 input = sys.stdin.readline
 
-g = int(input())
-p = int(input())
+def main():
+    g = int(input())
+    p = int(input())
+    parent = [0] * (g+1)
+    for i in range(g+1): parent[i] = i
+    answer = 0
+    for _ in range(p):
+        n = int(input())
+        tmp = find_parent(parent,n) 
+        if tmp == 0:break
+        answer += 1
+        parent[tmp] -= 1
+    print(answer)
 
-docking_start_gate = [0] * (g+1)
-gate = [False] * (g+1)
-for i in range(1,g+1):
-    docking_start_gate[i] = i
-answer = 0
+def find_parent(parent,x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x]
 
-for _ in range(p):
-    airplane = int(input())
-    for i in range(docking_start_gate[airplane],-1,-1):
-        if gate[i] == False:
-            gate[i] = True
-            answer += 1
-            docking_start_gate[airplane] = i-1
-            break
-    if gate[0] == True:
-        answer -= 1
-        break
-    
-
-print(answer)
+if __name__  == "__main__":
+    main()
