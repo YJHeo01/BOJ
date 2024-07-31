17fb83e941a401a89ed8c0ebba7834d8b04a9645
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-07-31 14:54:03 +0900

    Update 소문난 칠공주 - 1941번.py

--- 소문난 칠공주 - 1941번.py
+++ 소문난 칠공주 - 1941번.py
@@ -1,35 +1,48 @@
 #https://www.acmicpc.net/problem/1941
-#https://www.acmicpc.net/source/81840338
+#https://www.acmicpc.net/submit/1941/81842878
 
 from collections import deque
 from itertools import combinations
 
 def main():
     student = [list(input()) for _ in range(5)]
+    data = get_data()
+    test_case_list = list(combinations(data,7))
+    answer = 0
+    for test_case in test_case_list:
+        answer += solution(student,test_case)
+    print(answer)
+
+def get_data():
     data = []
     for x in range(5):
         for y in range(5):
             data.append((x,y))
-    test_case_list = list(combinations(data,7))
-    answer = 0
-    for test_case in test_case_list:
-        S_cnt = 0
-        for x,y in test_case:
-            if student[x][y] == 'S': S_cnt += 1
-        if S_cnt <= 3: continue
-        visited = [[True]*5 for _ in range(5)]
-        for x,y in test_case: visited[x][y] = False
-        bfs(visited,test_case[0])
-        for x,y in test_case:
-            if visited[x][y] == False:
-                answer -= 1
-                break
-        answer += 1
-    print(answer)
+    return data
 
-def bfs(vistied,start):
+def solution(student,test_case):
+    if get_S_cnt(student, test_case) <= 3: return 0
+    
+    area = [[False]*5 for _ in range(5)]
+    
+    for x,y in test_case: area[x][y] = True
+    
+    bfs(area,test_case[0])
+    
+    for x,y in test_case:
+        if area[x][y] == True: return 0
+    
+    return 1
+
+def get_S_cnt(student,test_case):
+    ret_value = 0
+    for x,y in test_case:
+        if student[x][y] == 'S': ret_value += 1
+    return ret_value
+
+def bfs(area,start):
     queue = deque([start])
-    vistied[start[0]][start[1]] = True
+    area[start[0]][start[1]] = False
     dx = [0,1,0,-1]
     dy = [1,0,-1,0]
     while queue:
@@ -37,10 +50,9 @@ def bfs(vistied,start):
         for i in range(4):
             nx = vx + dx[i]
             ny = vy + dy[i]
-            if nx < 0 or ny < 0 or nx >= 5 or ny >= 5 or vistied[nx][ny] == True: continue
-            vistied[nx][ny] = True
+            if nx < 0 or ny < 0 or nx >= 5 or ny >= 5 or area[nx][ny] == False: continue
+            area[nx][ny] = False
             queue.append((nx,ny))
 
 if __name__ == "__main__":
-    INF = int(1e9)
     main()
