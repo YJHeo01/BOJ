3e604112cbec7ddaf2f76f639f7f5198513c39f6
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-09-06 15:00:29 +0900

    Update 연료 채우기 - 1826번.py

--- 연료 채우기 - 1826번.py
+++ 연료 채우기 - 1826번.py
@@ -1,5 +1,5 @@
 #https://www.acmicpc.net/problem/1826
-#https://www.acmicpc.net/source/83514248
+#https://www.acmicpc.net/source/83514697
 
 import sys, heapq
 
@@ -8,30 +8,25 @@ input = sys.stdin.readline
 def main():
     n = int(input())
     array = sorted([list(map(int,input().split())) for _ in range(n)])
+    answer = solution(array)
+    print(answer)
+
+def solution(array):
     l,p = map(int,input().split())
+    array += [[l,0]]
     q = []
-    impossible = False
     cnt = 0
     for a,b in array:
         while True:
             if p >= a: break
-            if q == []:
-                impossible = True
-                break
+            if q == []: return -1
             energy = heapq.heappop(q)
             cnt += 1
             p -= energy
         heapq.heappush(q,-b)
-    while True:
-        if p >= l: break
-        if q == []:
-            impossible = True
-            break
-        energy = heapq.heappop(q)
-        cnt += 1
-        p -= energy
-    if impossible == True: cnt = -1
-    print(cnt)
+    
+    return cnt
+    
 
 if __name__ == "__main__":
     main()
