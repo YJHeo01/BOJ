025d1399b99cde295ae1cd76eba3812466ace4b4
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-10-08 11:02:36 +0900

    Create 차이를 M 이상으로 - 32396번.py

---
+++ 차이를 M 이상으로 - 32396번.py
@@ -0,0 +1,12 @@
+#https://www.acmicpc.net/problem/32396
+#https://www.acmicpc.net/source/84903940
+
+n,m = map(int,input().split())
+INF = int(1e13)
+array = list(map(int,input().split()))
+answer = 0
+for i in range(1,n):
+    if m > abs(array[i]-array[i-1]):
+        array[i] += INF
+        answer += 1
+print(answer)
