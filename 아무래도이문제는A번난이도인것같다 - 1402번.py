cb55356a176bf607e71c3b08bbd76365a1399168
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-11-04 10:38:51 +0900

    Create 아무래도이문제는A번난이도인것같다 - 1402번.py

---
+++ 아무래도이문제는A번난이도인것같다 - 1402번.py
@@ -0,0 +1,3 @@
+#https://www.acmicpc.net/problem/1402
+#a_i에 중복된 정수가 들어갈 수 없다는 제한이 존재하지 않고, 2번 이상 변환해도 변할 수 있다는 판정을 받을 수 있기 때문에, a_i에 1을 중복으로 넣다보면 언젠가는 변환에 성공할 수 밖에 없다.
+for _ in range(int(input())):print('yes')
