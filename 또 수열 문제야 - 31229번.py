882b8b20c6dc222e0a17cfbe145f99a2f7a58f54
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2024-12-05 15:54:25 +0900

    Create 또 수열 문제야 - 31229번.py

---
+++ 또 수열 문제야 - 31229번.py
@@ -0,0 +1,4 @@
+#https://www.acmicpc.net/problem/31229
+#https://www.acmicpc.net/source/87192172
+
+for i in range(int(input())):print(3+i*2,end=" ") #홀수의 곱은 홀수, 홀수의 합은 짝수이므로 모든 원소가 홀수일 경우 문제의 조건을 충족한다.
